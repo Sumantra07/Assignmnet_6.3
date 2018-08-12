b930a5ea8f2316bdc61c36263ac43889d0868550
Bhattacharya <sumantra.bhattacharya@siemens.com>
2018-08-12 13:35:15 +0530

    Assignmnet_6.3 JavaScript basics

---
+++ script.js
@@ -0,0 +1,10 @@
+var temperatureInCelsius = 40;
+var celsiusToFahrenheit;
+/* converting celsius to fahrenheit */
+celsiusToFahrenheit = (temperatureInCelsius * 9) / 5 + 32;
+console.log("Temparature of " + temperatureInCelsius + " celcius will be " + celsiusToFahrenheit + " fahrenheit");
+var temperatureInFahrenheit = 104;
+var fahrenheitToCelsius;
+/*converting fahrenheit to celsius*/
+fahrenheitToCelsius = ((temperatureInFahrenheit - 32) * 5) / 9;
+console.log("Temparature of " + temperatureInFahrenheit + " fahrenheit will be " + fahrenheitToCelsius + " fahrenheit");
